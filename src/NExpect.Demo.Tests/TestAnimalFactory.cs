c248b606acaa318770c94ba913bf7c3c444dc8dc
Davyd McColl <davydm@gmail.com>
2017-08-10 20:47:55 +0200

    feat: CreateBirds returns birds in order         -> dev has the choice to hang off of IA<T> vs IAn<T>         -> testing code is:                 - communicative                 - easy to grok                 - re-usable

--- src/NExpect.Demo.Tests/TestAnimalFactory.cs
+++ src/NExpect.Demo.Tests/TestAnimalFactory.cs
@@ -4,6 +4,7 @@ using NExpect.Interfaces;
 using NExpect.MatcherLogic;
 using NUnit.Framework;
 using System.Linq;
+using System.Reflection;
 using static NExpect.Expectations;
 
 namespace NExpect.Demo.Tests
@@ -26,6 +27,24 @@ namespace NExpect.Demo.Tests
             Expect(result).To.Be.A.Flamingo();
         }
 
+        [Test]
+        public void CreateBirds_ShouldReturnAFlamingoAndOstrichAndPenguin_InOrder()
+        {
+            // Arrange
+            var sut = Create();
+
+            // Pre-Assert
+
+            // Act
+            var result = sut.CreateBirds();
+
+            // Assert
+            Expect(result.Length).To.Equal(3);
+            Expect(result[0]).To.Be.A.Flamingo();
+            Expect(result[1]).To.Be.An.Ostrich();
+            Expect(result[2]).To.Be.A.Penguin();
+        }
+
         private static IAnimalFactory Create()
         {
             return new AnimalFactory();
@@ -34,18 +53,63 @@ namespace NExpect.Demo.Tests
 
     public static class MatcherExtensions
     {
-        public static void Flamingo<T>(this IA<T> an)
+        public static void Flamingo<T>(this IA<T> a)
         {
-            an.AddMatcher(item =>
+            a.AddMatcher(item =>
             {
                 var asFlamingo = item as Flamingo;
                 var passed = asFlamingo != null &&
                              asFlamingo.Legs == 2 &&
+                             asFlamingo.Habitat == Habitats.Wetlands &&
                              asFlamingo.Colors.Contains(Colors.Pink);
-                var not = passed ? "" : "not ";
+                var not = passed
+                    ? ""
+                    : "not ";
                 return new MatcherResult(
                     passed,
                     $"Expected {item} {not}to be a flamingo"
+                );
+            });
+        }
+
+        public static void Penguin<T>(this IA<T> a)
+        {
+            a.AddMatcher(item =>
+            {
+                var asPenguin = item as Penguin;
+                var passed = asPenguin != null &&
+                             asPenguin.Legs == 2 &&
+                             asPenguin.Habitat == Habitats.Polar &&
+                             asPenguin.Colors.Contains(Colors.Black) &&
+                             asPenguin.Colors.Contains(Colors.White) &&
+                             !asPenguin.Colors.Contains(Colors.Pink);
+                var not = passed
+                    ? ""
+                    : "not ";
+                return new MatcherResult(
+                    passed,
+                    $"Expected {item} {not}to be a penguin"
+                );
+            });
+        }
+
+        public static void Ostrich<T>(this IAn<T> an)
+        {
+            an.AddMatcher(item =>
+            {
+                var asOstrich = item as Ostrich;
+                var passed = asOstrich != null &&
+                             asOstrich.Legs == 2 &&
+                             asOstrich.Habitat == Habitats.Savanna &&
+                             asOstrich.Colors.Contains(Colors.Black) &&
+                             asOstrich.Colors.Contains(Colors.White) &&
+                             !asOstrich.Colors.Contains(Colors.Pink);
+                var not = passed
+                    ? ""
+                    : "not ";
+                return new MatcherResult(
+                    passed,
+                    $"Expected {item} {not}to be an ostrich"
                 );
             });
         }
--- src/NExpect.Demo/AnimalFactory.cs
+++ src/NExpect.Demo/AnimalFactory.cs
@@ -6,13 +6,24 @@ namespace NExpect.Demo
     public interface IAnimalFactory
     {
         Animal CreateFlamingo();
+        Animal[] CreateBirds();
     }
 
-    public class AnimalFactory: IAnimalFactory
+    public class AnimalFactory : IAnimalFactory
     {
         public Animal CreateFlamingo()
         {
             return new Flamingo();
+        }
+
+        public Animal[] CreateBirds()
+        {
+            return new Animal[]
+            {
+                new Flamingo(),
+                new Ostrich(),
+                new Penguin()
+            };
         }
     }
 }
